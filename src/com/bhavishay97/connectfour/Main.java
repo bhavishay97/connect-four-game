9e40c0378f7c33e61b0ae14a2dc02b5197902684
Bhavishay Chawla <bhavishay97@outlook.com>
2020-07-21 19:03:52 +0530

    Add menu bar

--- src/com/bhavishay97/connectfour/Main.java
+++ src/com/bhavishay97/connectfour/Main.java
@@ -4,7 +4,12 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.SeparatorMenuItem;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -19,6 +24,12 @@ public class Main extends Application {
 
         controller = loader.getController();
 
+        MenuBar menuBar = createMenu();
+        menuBar.prefWidthProperty().bind(primaryStage.widthProperty());
+
+        Pane menuPane = (Pane) rootGridPane.getChildren().get(0);
+        menuPane.getChildren().add(menuBar);
+
         Scene scene = new Scene(rootGridPane);
 
         primaryStage.setScene(scene);
@@ -27,6 +38,34 @@ public class Main extends Application {
         primaryStage.show();
     }
 
+    private MenuBar createMenu() {
+
+        // File menu
+        Menu fileMenu = new Menu("File");
+
+        MenuItem newGame = new MenuItem("New game");
+        MenuItem resetGame = new MenuItem("Reset game");
+        SeparatorMenuItem separatorMenuItem = new SeparatorMenuItem();
+        MenuItem exitGame = new MenuItem("Exit game");
+
+        fileMenu.getItems().addAll(newGame, resetGame, separatorMenuItem, exitGame);
+
+
+        // Help menu
+        Menu helpMenu = new Menu("Help");
+
+        MenuItem aboutGame = new MenuItem("About connect four");
+        SeparatorMenuItem separator = new SeparatorMenuItem();
+        MenuItem aboutMe = new MenuItem("About me");
+
+        helpMenu.getItems().addAll(aboutGame, separator, aboutMe);
+
+
+        MenuBar menuBar = new MenuBar();
+        menuBar.getMenus().addAll(fileMenu, helpMenu);
+
+        return menuBar;
+    }
 
     public static void main(String[] args) {
         launch(args);
