f2a45e30cc2cd357c5932a0f953eb3966978cf9b
Bhavishay Chawla <bhavishay97@outlook.com>
2020-07-22 11:22:27 +0530

    Create playground for game
    - Created a rectangle shape and then subtracted circles from it for the game playground area.

--- src/com/bhavishay97/connectfour/Controller.java
+++ src/com/bhavishay97/connectfour/Controller.java
@@ -5,11 +5,26 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
+
+    private static final int COLUMNS = 7;
+    private static final int ROWS = 6;
+    private static final int CIRCLE_DIAMETER = 80;
+    private static final String discColor1 = "#24303e";
+    private static final String discColor2 = "#4caa88";
+
+    private static String PLAYER_ONE = "Player 1";
+    private static String PLAYER_TWO = "Player 2";
+
+    private boolean isPlayer1Turn = true;
 
     @FXML
     public GridPane rootGridPane;
@@ -19,6 +34,31 @@ public class Controller implements Initializable {
 
     @FXML
     public Label playerNameLabel;
+
+    public void createPlayground() {
+        Shape rectangleWithHoles = getGameGrid();
+        rootGridPane.add(rectangleWithHoles, 0, 1);
+    }
+
+    private Shape getGameGrid() {
+        Shape rectangleWithHoles = new Rectangle((COLUMNS + 1) * CIRCLE_DIAMETER, (ROWS + 1) * CIRCLE_DIAMETER);
+
+        for (int row =0; row < ROWS; row++) {
+            for (int col =0; col < COLUMNS; col++) {
+                Circle circle = new Circle();
+                circle.setRadius(CIRCLE_DIAMETER / 2);
+                circle.setCenterX(CIRCLE_DIAMETER / 2);
+                circle.setCenterY(CIRCLE_DIAMETER / 2);
+
+                circle.setTranslateX(col * (CIRCLE_DIAMETER + 5) + (CIRCLE_DIAMETER / 4));
+                circle.setTranslateY(row * (CIRCLE_DIAMETER + 5) + (CIRCLE_DIAMETER / 4));
+                rectangleWithHoles = Shape.subtract(rectangleWithHoles, circle);
+            }
+        }
+
+        rectangleWithHoles.setFill(Color.WHITE);
+        return rectangleWithHoles;
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
